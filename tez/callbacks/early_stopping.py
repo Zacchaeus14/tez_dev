e7585f9d5919fa1f67cab0cce0e8d4f1bc0112ee
Zacchaeus14 <yw3642@nyu.edu>
2022-11-20 21:29:24 -0500

    add save total limit

--- tez/callbacks/early_stopping.py
+++ tez/callbacks/early_stopping.py
@@ -1,16 +1,18 @@
+import os
 import numpy as np
 from accelerate.logging import get_logger
 
 from tez import enums
 from tez.callbacks import Callback
 from tez.logger import logger
-
+from glob import glob
 
 logger = get_logger(__name__)
 
 
 class EarlyStopping(Callback):
-    def __init__(self, monitor, model_path, patience=5, mode="min", delta=0.001, save_weights_only=False):
+    def __init__(self, monitor, model_path, patience=5, mode="min", delta=0.001, save_weights_only=False,
+                 save_total_limit=1):
         self.monitor = monitor
         self.patience = patience
         self.counter = 0
@@ -28,12 +30,15 @@ class EarlyStopping(Callback):
 
         if self.monitor.startswith("train_"):
             self.model_state = "train"
-            self.monitor_value = self.monitor[len("train_") :]
+            self.monitor_value = self.monitor[len("train_"):]
         elif self.monitor.startswith("valid_"):
             self.model_state = "valid"
-            self.monitor_value = self.monitor[len("valid_") :]
+            self.monitor_value = self.monitor[len("valid_"):]
         else:
             raise Exception("monitor must start with train_ or valid_")
+        self.save_total_limit = save_total_limit
+        self.save_counter = 0
+        self.save_scores = {}
 
     def check(self, tez_trainer):
         epoch_score = tez_trainer.metrics[self.model_state][self.monitor_value]
@@ -70,5 +75,26 @@ class EarlyStopping(Callback):
             improvement_string = f"{self.val_score:.5f} -> {epoch_score:.5f}. Saving model!"
             logger.info(improvement_string)
             self.history.append(improvement_string)
-            tez_trainer.save(self.model_path, weights_only=self.save_weights_only)
+            if self.save_total_limit == 1:
+                tez_trainer.save(self.model_path, weights_only=self.save_weights_only)
+            else:
+                tez_trainer.save(f'{self.model_path}_{self.save_counter}', weights_only=self.save_weights_only)
+                self.save_scores[self.save_counter] = epoch_score
+                self.save_counter += 1
+                self.evict_checkpoint()
         self.val_score = epoch_score
+
+    def evict_checkpoint(self):
+        fps = glob(f'{self.model_path}_*')
+        if self.save_total_limit == -1 or fps <= self.save_total_limit:
+            return
+        counters = [int(fp.split('_')[-1]) for fp in fps]
+        scores = [self.save_scores[c] for c in counters]
+        cs = list(zip(counters, scores))
+        cs.sort(key=lambda x: x[1])
+        if self.mode == "min":
+            fp_del = f'{self.model_path}_{cs[-1][0]}'
+        else:
+            fp_del = f'{self.model_path}_{cs[0][0]}'
+        logger.info("Reach save total limit, deleting", fp_del)
+        os.remove(fp_del)
